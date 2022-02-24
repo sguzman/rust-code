37f4f8d7a9942d965d4ce24c860d7d90362560a1
Salvador Guzman <guzmansalv@gmail.com>
2022-02-24 13:58:04 -0800

    working code extraction of id

--- src/read_xml.rs
+++ src/read_xml.rs
@@ -5,29 +5,45 @@ use std::io::BufReader;
 
 use xml::reader::{EventReader, XmlEvent};
 
-fn indent(size: usize) -> String {
+fn indent(size: usize) -> String
+{
     const INDENT: &'static str = "    ";
-    (0..size).map(|_| INDENT)
-             .fold(String::with_capacity(size*INDENT.len()), |r, s| r + s)
+    (0..size)
+        .map(|_| INDENT)
+        .fold(
+            String::with_capacity(
+                size*INDENT.len()),
+                |r, s| r + s)
 }
 
-pub fn exec(path: &String) {
-        let file = File::open(path).unwrap();
+pub fn exec(path: &String)
+{
+    let file = File::open(path).unwrap();
     let file = BufReader::new(file);
 
     let parser = EventReader::new(file);
     let mut depth = 0;
-    for e in parser {
-        match e {
-            Ok(XmlEvent::StartElement { name, .. }) => {
-                println!("{}+{}", indent(depth), name);
-                depth += 1;
+    for e in parser
+    {
+        match e
+        {
+            Ok(XmlEvent::StartElement { name, attributes, .. }) =>
+            {
+                if name.local_name == "section"
+                {
+                    for attr in 0..(attributes.len())
+                    {
+                        if attributes[attr].name.local_name == "identifier"
+                        {
+                            println!("{:?}", attributes[attr].value);
+                            break;
+                        }
+
+                    }
+                }
             }
-            Ok(XmlEvent::EndElement { name }) => {
-                depth -= 1;
-                println!("{}-{}", indent(depth), name);
-            }
-            Err(e) => {
+            Err(e) =>
+            {
                 println!("Error: {}", e);
                 break;
             }
