3eceb751f585efb596059fa545f89fbf4dc3f76d
Salvador Guzman <guzmansalv@gmail.com>
2022-02-25 11:39:23 -0800

    working code extraction of id

--- src/read_xml/vec_id.rs
+++ src/read_xml/vec_id.rs
@@ -1,4 +1,13 @@
 pub fn exec(list: &Vec<xml::attribute::OwnedAttribute>) -> Option<String>
 {
+    for idx in 0..(list.len())
+    {
+        if list[idx].name.local_name == "identifier"
+        {
+            let obj: String = list[idx].value.clone();
+            return Some(obj);
+        }
+    }
+
     None
 }
